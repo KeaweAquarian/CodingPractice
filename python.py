632de796efa8d3feeae09ef264f3613cd2b6806d
KeaweA <keaweaquarian@gmail.com>
2023-08-21 11:16:31 -0400

    Initial practice with python

---
+++ python.py
@@ -0,0 +1,16 @@
+#Introductory practice with python
+
+#create a list
+numbers = [1,2,3,4,5]
+
+# Use a constructor
+numbers2 = list((1,2,3,4,4))
+
+fruits = ['apples', 'oranges', 'grapes','pears']
+
+print(fruits[1])
+print(len(fruits))
+
+fruits.append('Mangoes')
+
+print(fruits)
