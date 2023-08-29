1d397e8c218e30a41ba8f963e11668d3e0e4de66
KeaweA <keaweaquarian@gmail.com>
2023-08-29 17:13:14 -0400

    finalizing

--- Hangman.py
+++ Hangman.py
@@ -18,23 +18,26 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_lowercase)
     used_letters = set()
-
-    while len(word_letters) > 0:
+    i = 5
+    while (i) > 0:
         print("You have used these letters " , ' '.join(used_letters))
 
         word_list = [letter if letter in used_letters else '_' for letter in word]
         print("Current word: ", " ".join(word_list))
 
-        user_letter = input("Guess a letter ".lower())
+        user_letter = input("Guess a letter \n".lower())
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else: i -=1
 
         elif user_letter in used_letters:
-            print("You have already guessed that letter!")    
+            print("You have already guessed that letter!\n")    
         else:
-             print("I didn't understand that responce.")    
+             print("I didn't understand that responce.\n") 
+    if i == 0:
+        print(f"Good try, but you have run out of chances. The word was {word}\n")   
 
 hangman()
 
