773c3acac03875b6322429b74a88e1ea3c031131
KeaweA <keaweaquarian@gmail.com>
2023-08-30 13:00:18 -0400

    practice

--- python.py
+++ python.py
@@ -3,19 +3,45 @@
 #create a list
 numbers = [1,2,3,4,5]
 
-# Use a constructor
-numbers2 = list((1,2,3,4,4))
+# # Use a constructor
+# numbers2 = list((1,2,3,4,4))
 
-fruits = ['apples', 'oranges', 'grapes','pears']
+# fruits = ['apples', 'oranges', 'grapes','pears']
 
-print(fruits[1])
-print(len(fruits))
+# print(fruits[1])
+# print(len(fruits))
 
-fruits.append('Mangoes')
+# fruits.append('Mangoes')
 
-#alfkjalsdkfglaks
+# #alfkjalsdkfglaks
 
-print(fruits)
+# print(fruits)
 
-for i in range(len(fruits)):
-    print(fruits[i])
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+#4 types of primative values: int, str, bool, float
+
+# y = 1
+# z = "1"
+# print(str(y) + z)
+
+# age = input("how old are you.") always of the str type.
+
+# import math
+
+# pi = 3.6
+
+# p10 = 4
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi, 2))
+# print(math.sqrt(pi))
+# print(max(pi, p10))
+# print(min(pi, p10))
+
+#slicing str
+
+
